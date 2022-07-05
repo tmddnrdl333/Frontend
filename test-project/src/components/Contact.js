40ed2a8dfed57550ec5c76910d2d4ef03a0178e1
SeungNong <tmddnrdl333@naver.com>
2022-07-05 23:53:44 +0900

    remove, edit finished

--- test-project/src/components/Contact.js
+++ test-project/src/components/Contact.js
@@ -56,6 +56,9 @@ export default class Contact extends React.Component {
     });
   }
   handleRemove() {
+    if (this.state.selectedKey < 0) {
+      return;
+    }
     this.setState({
       contactData: update(this.state.contactData, {
         $splice: [[this.state.selectedKey, 1]],
@@ -105,16 +108,11 @@ export default class Contact extends React.Component {
         <ContactDetails
           isSelected={this.state.selectedKey !== -1}
           contact={this.state.contactData[this.state.selectedKey]}
+          onRemove={this.handleRemove}
+          onEdit={this.handleEdit}
         />
         <ContactCreate onCreate={this.handleCreate} />
       </div>
     );
   }
 }
-
-ContactDetails.defaultProps = {
-  contact: {
-    name: "",
-    phone: "",
-  },
-};
