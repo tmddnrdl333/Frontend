a87867d1e1c9fc35c19d6b3390b989b664f9cb35
SeungNong <tmddnrdl333@naver.com>
2022-07-27 00:26:43 +0900

    Create: Vue3 Project Created

---
+++ Vue3/vue/project/src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Hello {{ userName }}~!!</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: 'John Doe',
+      message: 'Welcome 존경하는 스님',
+      arr: [],
+      obj: {}
+    }
+  }
+}
+</script>
+<style scoped>
+.text-red {
+  color: red;
+}
+</style>
